848533f82d504a7a934a7393d1d2f66cce7d99cf
rafaelvaloes <rafael.colnago@live.com>
2024-10-10 21:55:10 -0300

    Aula 6

---
+++ objects/script.js
@@ -0,0 +1,7 @@
+const user = {
+    id: 1,
+    name: "Rafael",
+    birthdate: "30 de Abril de 1997",
+    logged: false,
+}
+console.log (user)
---
+++ variable/script.js
@@ -0,0 +1,7 @@
+var value1 = 1;
+
+console.log (value1)
+
+let value2 = 2;
+
+console.log (value2);
